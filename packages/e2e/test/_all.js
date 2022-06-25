8d04be8bee2b8f72baa187834f610ba194dc161f
Le Vivilet <levivlet@pm.me>
2022-06-25 18:16:08 +0200

    try to exit process on exit

--- packages/e2e/test/_all.js
+++ packages/e2e/test/_all.js
@@ -12,18 +12,24 @@ const getTestFiles = async () => {
 }
 
 const main = async () => {
-  state.runImmediately = false
-  await startAll()
-  console.info('SETUP COMPLETE')
-  const testFiles = await getTestFiles()
-  for (const testFile of testFiles) {
-    state.tests = []
-    await import(testFile)
-    for (const test of state.tests) {
-      await runTest(test)
+  try {
+    state.runImmediately = false
+    await startAll()
+    console.info('SETUP COMPLETE')
+    const testFiles = await getTestFiles()
+    for (const testFile of testFiles) {
+      state.tests = []
+      await import(testFile)
+      for (const test of state.tests) {
+        await runTest(test)
+      }
     }
+    await closeAll()
+  } catch (error) {
+    console.info('tests failed')
+    console.error(error)
+    process.exit(1)
   }
-  await closeAll()
 }
 
 main()
