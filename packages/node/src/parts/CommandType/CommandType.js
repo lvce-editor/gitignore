6fb139a6f661d6660c72fd9b9adba8cc13a68faa
Le Vivilet <levivlet@pm.me>
2023-12-25 21:43:26 +0100

    feature: add node package

---
+++ packages/node/src/parts/CommandType/CommandType.js
@@ -0,0 +1,1 @@
+export const Exec = 'Exec.exec'
---
+++ packages/node/src/parts/ExtensionHost/ExtensionHostCommandGitignoreAdd.js
@@ -0,0 +1,34 @@
+import * as Github from '../Github/Github.js'
+import * as Download from '../Download/Download.js'
+import { join } from 'path'
+
+export const id = 'gitignore.add'
+
+const toPick = (gitIgnoreFile) => {
+  return {
+    label: gitIgnoreFile.label,
+  }
+}
+
+const getPicks = async () => {
+  const gitignoreFiles = await Github.getGetGitIgnoreFiles()
+  return gitignoreFiles
+}
+
+export const execute = async () => {
+  const selectedPick = await vscode.showQuickPick({
+    getPicks,
+    toPick,
+  })
+  if (!selectedPick) {
+    return
+  }
+  const url = selectedPick.url
+
+  const workspaceFolder = vscode.getWorkspaceFolder()
+  const gitignorePath = join(workspaceFolder, '.gitignore')
+  // TODO download it to the current workspace
+  await Download.download(url, gitignorePath)
+
+  vscode.showNotification('info', 'file created successfully')
+}
