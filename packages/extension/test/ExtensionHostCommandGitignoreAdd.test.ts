143b4921de1441b18a54be93025713ba6d349e38
Le Vivilet <levivlet@pm.me>
2024-09-08 19:09:22 +0200

    jest globals

--- packages/extension/test/ExtensionHostCommandGitignoreAdd.test.ts
+++ packages/extension/test/ExtensionHostCommandGitignoreAdd.test.ts
@@ -1,4 +1,5 @@
 import * as ExtensionHostCommandGitignoreAdd from '../src/parts/ExtensionHost/ExtensionHostCommandGitignoreAdd.ts'
+import { test, expect } from '@jest/globals'
 
 test('id', () => {
   expect(ExtensionHostCommandGitignoreAdd.id).toEqual(expect.any(String))
