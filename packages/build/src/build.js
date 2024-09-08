935dc18e55018ce37733efe04fd200e94289d217
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-09-08 19:03:52 +0200

    feature: add build folder (#30)

---
+++ packages/build/src/build.js
@@ -0,0 +1,47 @@
+import { bundleJs, packageExtension } from '@lvce-editor/package-extension'
+import fs, { readFileSync } from 'node:fs'
+import path, { join } from 'node:path'
+import { root } from './root.js'
+
+const extension = path.join(root, 'packages', 'extension')
+
+fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
+
+fs.mkdirSync(path.join(root, 'dist'))
+
+const packageJson = JSON.parse(
+  readFileSync(join(extension, 'package.json')).toString(),
+)
+delete packageJson.xo
+delete packageJson.jest
+delete packageJson.prettier
+delete packageJson.devDependencies
+
+fs.writeFileSync(
+  join(root, 'dist', 'package.json'),
+  JSON.stringify(packageJson, null, 2) + '\n',
+)
+fs.copyFileSync(join(root, 'README.md'), join(root, 'dist', 'README.md'))
+fs.copyFileSync(join(root, 'LICENSE'), join(root, 'dist', 'LICENSE'))
+// fs.copyFileSync(
+//   join(root, 'ThirdPartyNotices.txt'),
+//   join(root, 'dist', 'ThirdPartyNotices.txt'),
+// )
+fs.copyFileSync(
+  join(extension, 'extension.json'),
+  join(root, 'dist', 'extension.json'),
+)
+fs.cpSync(join(extension, 'src'), join(root, 'dist', 'src'), {
+  recursive: true,
+})
+
+await bundleJs(
+  join(root, 'dist', 'src', 'gitignoreMain.js'),
+  join(root, 'dist', 'dist', 'gitignoreMain.js'),
+)
+
+await packageExtension({
+  highestCompression: true,
+  inDir: join(root, 'dist'),
+  outFile: join(root, 'extension.tar.br'),
+})
