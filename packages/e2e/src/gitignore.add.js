a370c100f125961a1b8f64ebd2b4e07e5c3ce7aa
Le Vivilet <levivlet@pm.me>
2024-09-08 19:17:19 +0200

    feature: add another e2e test

---
+++ packages/e2e/src/gitignore.add.js
@@ -0,0 +1,16 @@
+export const name = 'gitignore.error'
+
+export const skip = true
+
+export const test = async ({
+  FileSystem,
+  Workspace,
+  Settings,
+  SideBar,
+  KeyBoard,
+  Locator,
+  expect,
+}) => {
+  //  TODO mock network to return a not found response
+  // TODO verify helpful error message is displayed in that case
+}
