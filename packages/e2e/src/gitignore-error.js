35e8b18b2832452a2fced1c452e0fec96f636652
Le Vivilet <levivlet@pm.me>
2024-09-08 19:14:28 +0200

    feature: add e2e test

--- packages/e2e/src/gitignore-error.js
+++ packages/e2e/src/gitignore-error.js
@@ -1,7 +1,5 @@
 export const name = 'gitignore.error'
 
-export const skip = true
-
 export const test = async ({
   FileSystem,
   Workspace,
@@ -9,8 +7,17 @@ export const test = async ({
   SideBar,
   KeyBoard,
   Locator,
+  QuickPick,
   expect,
 }) => {
-  //  TODO mock network to return a not found response
+  await QuickPick.open()
+  await QuickPick.setValue('>Add gitignore')
+  await QuickPick.selectItem('Add gitignore')
+  const errorMessage = Locator('#DialogBodyErrorMessage')
+  await expect(errorMessage).toBeVisible()
+  await expect(errorMessage).toHaveText(
+    `Failed to execute command: Failed to get gitignore files: TypeError: Cannot read properties of null (reading 'cache')`,
+  )
+  // TODO mock network to return a not found response
   // TODO verify helpful error message is displayed in that case
 }
